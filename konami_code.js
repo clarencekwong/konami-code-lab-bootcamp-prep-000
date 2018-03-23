ee93ecc386875a21bf46817385bc387da8d3bed3
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-23 17:40:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0
+let index = 0;
 
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -16,4 +16,5 @@ function init(e) {
   }
 }
 
-const divs = document.
+const body = document.body;
+body.addEventlistener('keydown', init);
