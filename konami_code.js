a6c3a111dda98a7b8528efc8f6a341fd9f9ad0c9
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-23 17:52:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-let index = 0;
-
 function init(e) {
   // Write your JavaScript code inside the init() function
+  let index = 0;
   document.body.addEventlistener('keydown', function (e){
-    
-  })
   const key = parseInt(e.detail || e.which);
   if (key === code[index]) {
     index++;
     if (index === code.length) {
-      alert("Congratulation, the konami code has been submitted");
+      alert("Congratulation, the konami code has been submitted.");
       index = 0;
     }
   } else {
     index = 0;
   }
+})
 }
-
-const body = document.body;
-body.addEventlistener('keydown', init);
