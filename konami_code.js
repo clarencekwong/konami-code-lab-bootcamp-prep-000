86292c6f195acc84e608eb276fdb8b6945432106
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-23 17:35:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
   if (key === code[index]) {
     index++;
     if (index === code.length) {
-      alert("")
+      alert("Congratulation, the konami code has been submitted")
     }
   }
 }
